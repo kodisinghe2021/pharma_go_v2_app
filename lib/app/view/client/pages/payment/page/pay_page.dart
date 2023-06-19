30e0f8a5d689125207a1efa61777faf465ace3ff
Prabhath Kodisinghe <81280190+kodisinghe2021@users.noreply.github.com>
2023-06-19 22:48:10 +0530

    1

--- lib/app/view/client/pages/payment/page/pay_page.dart
+++ lib/app/view/client/pages/payment/page/pay_page.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pharma_go_v2_app/supports/constant/box_constraints.dart';
 
 class PayPage extends GetView {
   const PayPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pay Here'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pay Here'),
+        ),
+        body: Container(
+          width: getscreenSize(context).width,
+          height: double.maxFinite,
+          color: Colors.cyan,
+        ),
       ),
     );
   }
